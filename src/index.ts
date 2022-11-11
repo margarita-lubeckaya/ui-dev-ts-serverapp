06a7300ac17ebecfb5ac4f3da1e4a536906959cd
margarita.lubeckaya <margarita.lubeckaya@gmail.com>
2022-11-11 17:21:56 +0300

    add dynamic route

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,41 @@ import http, {IncomingMessage, ServerResponse} from "http";
 import path from "path";
 import fs from "fs/promises";
 import url from "url";
+import fetch from "node-fetch"
+
+interface Joke {
+    id: string
+    joke: string
+    status: number
+}
 
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
 
     const parseUrl = url.parse(req.url || "")
+    const responsePath = parseUrl.pathname === '/' ? '/index' : parseUrl.pathname
 
     let data = ''
     try {
-        const responsePath = parseUrl.pathname === '/' ? '/index' : parseUrl.pathname
         const filePath = path.join(__dirname, `static${responsePath}.html`)
-        console.log('filePath', filePath)
         data = await fs.readFile(filePath, 'utf-8');
     } catch {
         const errorPath = path.join(__dirname, `static/404.html`)
         data = await fs.readFile(errorPath, 'utf-8');
+    }
+
+    if (responsePath === '/dad-joke') {
+
+        const response = await fetch('https://icanhazdadjoke.com', {
+            headers: {
+                accept: "application/json",
+                "user-agent": "NodeJS Server",
+            }
+        })
+
+        const joke : Joke = await response.json()
+
+        console.log(joke)
+        data = data.replace(/{{joke}}/gm, joke.joke)
     }
 
     res.writeHead(200, {
